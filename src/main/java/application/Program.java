8c60d69491cc41710dc2ee6ecb42fa0a243b9438
Junior Vieira <junior.vieira.1990@gmail.com>
2022-04-30 18:16:04 -0300

    Seller entity class

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -2,6 +2,10 @@ package application;
 
 import db.DB;
 import model.entities.Department;
+import model.entities.Seller;
+import org.joda.time.DateTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
 
 import java.sql.Connection;
 
@@ -10,6 +14,12 @@ public class Program {
     public static void main(String[] args) {
 
         Department d1 = new Department(1, "Computers");
-        System.out.println(d1);
+
+        DateTimeFormatter fmt = DateTimeFormat.forPattern("dd/MM/yyyy");
+        DateTime dt1 = fmt.parseDateTime("15/12/1980");
+
+        Seller s1 = new Seller(1, "Bob Brown", "bob@gmail.com", dt1, 1000.0, d1);
+
+        System.out.println(s1);
     }
 }
