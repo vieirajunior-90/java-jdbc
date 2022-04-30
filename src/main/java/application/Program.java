b090caa34da55442d613b87af8f63228fbad0889
Junior Vieira <junior.vieira.1990@gmail.com>
2022-04-30 20:22:46 -0300

    Running script.sql to create tables

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,25 +1,32 @@
 package application;
 
 import db.DB;
-import model.entities.Department;
-import model.entities.Seller;
-import org.joda.time.DateTime;
-import org.joda.time.format.DateTimeFormat;
-import org.joda.time.format.DateTimeFormatter;
+import org.apache.ibatis.jdbc.ScriptRunner;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.Reader;
 import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Program {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 
-        Department d1 = new Department(1, "Computers");
+        Connection connection = DB.getConnection();
+        Statement st = connection.createStatement();
 
-        DateTimeFormatter fmt = DateTimeFormat.forPattern("dd/MM/yyyy");
-        DateTime dt1 = fmt.parseDateTime("15/12/1980");
+        try {
+            st.executeQuery("SELECT * FROM department");
+        }
+        catch (SQLException e) {
+            ScriptRunner sr = new ScriptRunner(connection);
+            Reader reader = new BufferedReader(new FileReader("src/main/resources/script.sql"));
+            sr.runScript(reader);
+        }
 
-        Seller s1 = new Seller(1, "Bob Brown", "bob@gmail.com", dt1, 1000.0, d1);
 
-        System.out.println(s1);
+
     }
 }
