b105e60c5c6e6752b640d2ee4d6942e246660b93
Junior Vieira <junior.vieira.1990@gmail.com>
2022-05-04 19:26:25 -0300

    Insert Department Implementation

--- src/main/java/model/dao/DaoFactory.java
+++ src/main/java/model/dao/DaoFactory.java
@@ -1,12 +1,15 @@
 package model.dao;
 
 import db.DB;
+import model.dao.impl.DepartmentDaoJDBC;
 import model.dao.impl.SellerDaoJDBC;
-import model.entities.Seller;
 
 public class DaoFactory {
 
     public static SellerDao createSellerDao() {
         return new SellerDaoJDBC(DB.getConnection());
+    }
+    public static DepartmentDao createDepartmentDao() {
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
