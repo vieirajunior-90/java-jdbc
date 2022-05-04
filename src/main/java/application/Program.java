2351bc6c3cd841085ef10223b9c67a0d58f933e2
Junior Vieira <junior.vieira.1990@gmail.com>
2022-05-04 16:12:50 -0300

    Insert Implementation

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -4,6 +4,8 @@ import model.dao.DaoFactory;
 import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
 
 import java.util.List;
 
@@ -29,5 +31,17 @@ public class Program {
         for (Seller obj : allSellers) {
             System.out.println(obj);
         }
+
+        System.out.println("\n==== INSERT SELLER ====");
+        Seller newSeller = new Seller(null,
+                "Juan Vasquez",
+                "juanvasquez@hotmail.com",
+                new DateTime(1980, 12, 12, 0, 0),
+                2000.0,
+                department);
+
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted! New id: " + newSeller.getId());
+
     }
 }
